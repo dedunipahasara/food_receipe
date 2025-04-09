361d9e4b21547749f0d823ca583a583444991a73
dedunipahasara <139765723+dedunipahasara@users.noreply.github.com>
2025-04-09 16:51:25 +0530

    add routing part for navigate

--- receipe/src/App.jsx
+++ receipe/src/App.jsx
@@ -1,13 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import React from 'react'
+import Navbar from "./components/Navbar.jsx";
+import About from "./pages/About.jsx";
+import Receipe from "./pages/Receipe.jsx";
 
 
 function App() {
- 
-
   return (
     <>
+    <div>
+      <Navbar/>
+    </div>
      
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} /> 
+        <Route path="/receipe" element={<Receipe />} /> 
+
+
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
